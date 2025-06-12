0f8766c8aa582570047aba2406c910fa7661a14f
vamanjain <vaman.jain17@gmail.com>
2025-06-12 17:47:49 +0530

    final

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,12 @@ const damion = Damion({
 const Navbar = () => {
     return (
         <nav className={` ${damion.className}` + " w-full h-50px  text-black p-4 flex justify-between items-center py-5 fixed top-0 left-0 backdrop-blur z-50"}>
-            <h1 className="text-5xl font-bold mx-auto">Moodie</h1>
+            <h1 className="text-5xl font-bold mx-auto">
+                <Link href="/" >
+                    Moodie
+                </Link>
+
+                </h1>
             <Link href="/admin" className="text-2xl font-bold text-teal-600">
                 Admin    
             </Link>
